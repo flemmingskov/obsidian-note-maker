52650843d64d14adf98619e5742b6c108e2061a0
Flemming Skov <fs@bios.au.dk>
2022-03-06 13:18:03 +0100

    Create config.py
    Add a configuration file - stores commonly used paths etc

---
+++ config.py
@@ -0,0 +1,12 @@
+## Setting paths to files and folders
+
+if (platform.system() == 'Darwin'):
+    primary_path = '/Users/flemmingskov/Desktop/obsidian/'
+else:
+    primary_path = 'C:/Users/au3406/Desktop/Obsidian/'
+
+# update to create list of possible workspaces to each page
+list_secondary_paths = ['rewilding/', 'bioMonit/']
+
+# variables related to layout and appearance
+expander_space = 2  # number of empty lines at the end of the expander
