58615e34be0fc2c08c4ef1a437f19529c74a91e0
Flemming Skov <fs@bios.au.dk>
2022-03-18 06:58:59 +0100

    Update tool.py
    Changes March 16

--- tool.py
+++ tool.py
@@ -1,32 +1,66 @@
 '''
 File: tool.py
 Author: Flemming Skov 
-Main streamlit/python file
 Start app from a terminal window typing: "streamlit run 'path_to_tool_folder'/tool.py
 Latest version: March 14 2022
 '''
 
-# Import libraries
+# IMPORT LIBRARIES
+##################
 import streamlit as st
-import os, time, datetime, sqlite3, platform, logging, itertools, re, math, sys, random
-import copy, zipfile
 import pandas as pd
 import numpy as np
+import datetime
+import platform
+import itertools
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 wnl = WordNetLemmatizer()
+# import os, time, sqlite3, logging, re, math, sys, random, copy, zipfile
 
+
+# SETTING VARIABLES
+###################
 time_stamp = datetime.datetime.now().strftime('(%Y,%b,%d)')  # time stamp
 imported_keywords_chosen = []
 private_keywords_chosen = []
 
-exec(open("./function_library.py").read())    
 
+# DEFINE FUNCTIONS
+#################
+def get_platform():
+    if (platform.system() == 'Darwin'):
+#        logging.info(' running MacOS ...')
+        mainwork = '/Users/flemmingskov/Desktop/'
+    else:
+ #       logging.info(' running Windows 10 ...')
+        mainwork = 'C:/Users/au3406/iCloudDrive/Desktop/'
+    return mainwork
+
+def clean_keyword (old_keyword):
+    composite_keyword = ''
+    old_keyword = old_keyword.replace("/", "_")
+    keyword = old_keyword.replace("-", " ")     # separating keywords joined by '-' in order to lemmatize and rejoin
+    for keyword_part in keyword.split(" "):
+        if keyword_part != "":
+            keyword_part = wnl.lemmatize(keyword_part.lower())
+            #if (synonyms.get(keyword_part, "none")) != 'none':   # check if part of keyword is a synonym
+            #    keyword_part = synonyms.get(keyword_part)              
+            if composite_keyword != "":
+                composite_keyword = composite_keyword + "-" + keyword_part
+            if composite_keyword == "":
+                composite_keyword = composite_keyword + keyword_part   
+            #if (synonyms.get(composite_keyword, "none")) != 'none':   # check if the new composite keyword is a synonym
+            #    composite_keyword = synonyms.get(composite_keyword)
+    if composite_keyword != '':
+        return composite_keyword
+
+
+# SIDEBAR LAYOUT
+################
 st.sidebar.title("Obsidian Note Maker")
 st.sidebar.info("Version 0.02 - March 14, 2022")
-
-# CREATE SIDEBAR AND CONTENTS
 st.sidebar.markdown("   ")
 st.sidebar.title("GitHub code")
 st.sidebar.info(
@@ -44,10 +78,12 @@ st.sidebar.info(
     Email: fs@ecos.au.dk   
     """)
 
-#________________________________________________________
+# MAIN PAGE LAYOUT
+##################
 st.title('Convert Web of Science data to Obsidian notes')
 
-# Expander for About & help ....
+# EXPANDER for About & help
+###########################
 with st.expander("Instructions"):
     st.subheader('From Web of Science export files to Obsidian .md files')
     st.markdown(
@@ -62,55 +98,60 @@ with st.expander("Instructions"):
         * Press '**Export**' to process Obsidian note files as a .zip  
     """)
 
-st.subheader('1 - upload Web of Science export files in full .txt format')
+# UPLOAD FILES
+##############
+st.write(' - - -')
+st.subheader('-> upload WoS  files')
+st.markdown('Files should be __tab delimited__ with option __full record__ \
+    using the __Export__ button in Web of Science')
     
-# I - open and import file content(s) to dataframe wosIn_df
-uploaded_files = st.file_uploader("", accept_multiple_files=True)
+uploaded_wos_files = st.file_uploader("", accept_multiple_files=True)
 
-if not uploaded_files:
+if not uploaded_wos_files:
   st.warning('Please, upload one ore more Web of Science export files')
   st.stop()
 
-new_df = pd.DataFrame()
-wosIn_df = pd.DataFrame()
+# initializing data frames
+raw_wos_data_df = pd.DataFrame()
+processed_wos_data_df = pd.DataFrame()
 
-for uploaded_file in uploaded_files:
+for uploaded_file in uploaded_wos_files:
     print(' .. step I - importing: ')
     columnames = []
     for i in range(0, 69):
         columnames.append(str(i))
   
-    wosIn_df = pd.read_csv(uploaded_file, names=columnames,
+    raw_wos_data_df = pd.read_csv(uploaded_file, names=columnames,
                         index_col=False,
                         delimiter='\t',
                         skiprows=1)
     
     # The following works from February 2022 Web of Science (new fields added)                   
-    wosIn_df = wosIn_df[wosIn_df.columns[[1, 8, 19, 20, 21, 22, 25, 26,
+    raw_wos_data_df = raw_wos_data_df[raw_wos_data_df.columns[[1, 8, 19, 20, 21, 22, 25, 26,
                                         28, 33, 9, 45, 59, 61, 63, 55, 34, 35]]]
          
-    wosIn_df.replace(r'\s+', np.nan, regex=True).replace('', np.nan)
-    wosIn_df = wosIn_df.fillna('')
-    wosIn_df[100] = time_stamp    # New column with time stamp (today)
-    wosIn_df.columns = ['authors', 'title', 'kw1', 'kw2', 'abstr',
+    raw_wos_data_df.replace(r'\s+', np.nan, regex=True).replace('', np.nan)
+    raw_wos_data_df = raw_wos_data_df.fillna('')
+    raw_wos_data_df[100] = time_stamp    # New column with time stamp (today)
+    raw_wos_data_df.columns = ['authors', 'title', 'kw1', 'kw2', 'abstr',
                         'inst', 'email', 'autID', 'funding', 'cites',
                         'journal', 'year', 'wos_sub_cat1',
                         'wos_sub_cat2', 'wosid', 'doi', 'usc1', 'usc2', 'time_stamp']
-    new_df = pd.concat([new_df, wosIn_df])
+    processed_wos_data_df = pd.concat([processed_wos_data_df, raw_wos_data_df])
 
-    new_df = new_df.drop_duplicates(subset='wosid', keep='last')
-    new_df[["cites", "year", "usc1", "usc2"]] = new_df[["cites", "year", "usc1", "usc2"]].apply(pd.to_numeric, \
+    processed_wos_data_df = processed_wos_data_df.drop_duplicates(subset='wosid', keep='last')
+    processed_wos_data_df[["cites", "year", "usc1", "usc2"]] = processed_wos_data_df[["cites", "year", "usc1", "usc2"]].apply(pd.to_numeric, \
                         errors='coerce')
-    new_df.loc[new_df['year'] < 1966, 'year'] = np.nan
-    new_df = new_df.dropna(subset=['year', 'wosid'])
-    new_df = new_df.reset_index(drop=True)
-    new_df['year'] = new_df.year.astype(int)
-    new_df['cites'] = new_df.cites.astype(int)
+    processed_wos_data_df.loc[processed_wos_data_df['year'] < 1966, 'year'] = np.nan
+    processed_wos_data_df = processed_wos_data_df.dropna(subset=['year', 'wosid'])
+    processed_wos_data_df = processed_wos_data_df.reset_index(drop=True)
+    processed_wos_data_df['year'] = processed_wos_data_df.year.astype(int)
+    processed_wos_data_df['cites'] = processed_wos_data_df.cites.astype(int)
 
-# II - linking unique categories to WoS IDs        
+# linking unique categories to WoS IDs        
 wos_list, year_list, cat_list, cite_list, person_list, email_list, funding_list = ([] for i in range(7))
 
-for index, row in new_df.iterrows():
+for index, row in processed_wos_data_df.iterrows():
     wosid = row[14]
     yr = row[11]
     cat1list = row[12] #specific categories
@@ -125,30 +166,31 @@ for index, row in new_df.iterrows():
             cat_list.append(cat)
             cite_list.append(cites)
 
-links_df = pd.DataFrame({'wosid': wos_list, 'year': year_list,
+links_to_categories_df = pd.DataFrame({'wosid': wos_list, 'year': year_list,
                     'category': cat_list, 'cites': cite_list})
-links_df = links_df[1:]
-links_df = links_df[['wosid', 'year', 'cites', 'category']]
-dep_links_df = links_df
+links_to_categories_df = links_to_categories_df[1:]
+links_to_categories_df = links_to_categories_df[['wosid', 'year', 'cites', 'category']]
+dep_links_df = links_to_categories_df
 dep_links_df = dep_links_df.drop_duplicates(keep='last')
 
 
-# II -  creating a keyword vocabulary
-st.subheader('2 - list of keywords from uploaded files')
+# SHOW KEYWORDS FROM UPLOADED FILES
+###################################
+st.write(' - - -')
+st.subheader('-> keywords in uploaded files')
 
-searchCriteria = """(links_df.category != 'dummyCriteria')"""   ## If necessary to select subset
-subset = (dep_links_df.loc[eval(searchCriteria), ['wosid']]
+search_criteria = """(links_to_categories_df.category != 'dummyCriteria')"""
+selected_records = (dep_links_df.loc[eval(search_criteria), ['wosid']]
                 .drop_duplicates(['wosid']))
-dataIn = (pd.merge(subset, new_df, on='wosid', how='inner')[['authors', 'title', 
+extract_kewords_from_df = (pd.merge(selected_records, processed_wos_data_df, on='wosid', how='inner')[['authors', 'title', 
                 'kw1', 'kw2', 'abstr', 'inst', 'email', 'autID', 'funding', 
                 'cites', 'journal', 'year', 'wos_sub_cat1', 'wos_sub_cat2', 
                 'wosid', 'time_stamp']]  )
 
-#synonyms = import_synonyms()
 (wosid_list, year_list, orig_list, clean_list, comb_list, keyword_list) = \
     ([], [], [], [], [], [])
 
-for index, row in dataIn.iterrows():  # Iterate over all rows in dataframe
+for index, row in extract_kewords_from_df.iterrows():  # Iterate over all rows in dataframe
     concat_clean = []
 
     if row[2] != '':
@@ -182,9 +224,10 @@ f"""Total number of keywords is {total_count}. Number of keywords with two or
 more occurrences: {ones}. Number of keywords with three or 
 more occurrences: {twos}""") 
 
-if st.checkbox("Show all keywords from uploaded files"):
+with st.expander("Show all keywords from uploaded files"):
     st.dataframe(keyword_count)
-
+#if st.checkbox("Show all keywords from uploaded files"):
+    
 imported_keyword_series = keyword_count['keyword'].squeeze()
 imported_keywords_default = imported_keyword_series[0:45]
 imported_keywords_options = imported_keyword_series[0:200]
@@ -194,7 +237,12 @@ imported_keywords_chosen = st.multiselect(
     imported_keywords_options,
     imported_keywords_default)
 
-st.subheader('3 - additional list of private keywords')
+
+# SHOW PRIVATE KEYWORDS
+#######################
+st.write(' - - -')
+st.subheader('-> private keywords')
+st.markdown('Add optionally a list of __private keywords__ to the searchoptionally ')
 uploaded_keywords = st.file_uploader("", accept_multiple_files=False)
 if not uploaded_keywords:
   st.warning('No private keywords uploaded')
@@ -207,7 +255,12 @@ else:
 final_list_of_keywords = list(set(imported_keywords_chosen + private_keywords_chosen))
 final_list_of_keywords.sort()
 
-st.subheader('4 - combined list of keywords')
+
+# SHOW COMBINED KEYWORDS
+########################
+st.write(' - - -')
+st.subheader('-> combined keywords')
+st.markdown('Final list of combined WoS and private keywords - sorted alphabetically')
 final_keywords_chosen = st.multiselect(
     'Add or delete keywords from list',
     final_list_of_keywords,
